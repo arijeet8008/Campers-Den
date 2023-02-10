09df42a2c4aa031c19ab1ce179a4c74be5f44928
arijeet@8008 <arijeetchowdhury8008@gmail.com>
2023-02-11 01:59:25 +0530

    payment,cart,product update 1.1

--- CampersDen/src/main/java/com/campersDen/service/PaymentService.java
+++ CampersDen/src/main/java/com/campersDen/service/PaymentService.java
@@ -1,10 +1,12 @@
 package com.campersDen.service;
 
+import com.campersDen.exception.CustomerException;
+import com.campersDen.exception.OrdersException;
 import com.campersDen.exception.PaymentException;
 import com.campersDen.model.Payment;
 
 public interface PaymentService {
 
-	public Payment finalPayment(Payment payment) throws PaymentException;
+	public Payment finalPayment(Payment payment,Integer orderId,Integer customerId) throws PaymentException,CustomerException,OrdersException;
 	
 }
