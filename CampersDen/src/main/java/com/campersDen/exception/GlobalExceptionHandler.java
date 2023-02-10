74c544ce7954f69e303dab650e2dc2809a75ecb7
arijeet@8008 <arijeetchowdhury8008@gmail.com>
2023-02-10 22:56:29 +0530

    payment added

--- CampersDen/src/main/java/com/campersDen/exception/GlobalExceptionHandler.java
+++ CampersDen/src/main/java/com/campersDen/exception/GlobalExceptionHandler.java
@@ -12,6 +12,20 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+	
+	
+	@ExceptionHandler(PaymentException.class)
+	public ResponseEntity<MyErrorDetails> paymentNotFoundException(PaymentException ge, WebRequest req) {
+
+		MyErrorDetails err = new MyErrorDetails();
+
+		err.setTimestamp(LocalDateTime.now());
+		err.setMessage(ge.getMessage());
+		err.setDetails(req.getDescription(false));
+
+		return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+
+	}
 
 	@ExceptionHandler(ProductsException.class)
 	public ResponseEntity<MyErrorDetails> productNotFoundException(ProductsException ge, WebRequest req) {
--- CampersDen/src/main/java/com/campersDen/model/Payment.java
+++ CampersDen/src/main/java/com/campersDen/model/Payment.java
@@ -1,5 +1,33 @@
 package com.campersDen.model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Payment {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer paymentId;
+	
+	private TransitionMode mode;
+	
+	private Double totalOrdervalue;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private Orders orders;
+	
 }
